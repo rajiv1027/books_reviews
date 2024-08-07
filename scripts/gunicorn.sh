ae821a00c9e4595b0c70cedb4e1ae9b9b168b340
rrsingh <rsingh@infoane.com>
2024-08-07 22:45:39 +0530

    Updated code

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
-sudo cp /home/ubuntu/jktech/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
-sudo cp /home/ubuntu/jktech/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
+sudo cp /home/ubuntu/jktech/gunicorn/gunicorn.socket /etc/systemd/system/gunicorn.socket
+sudo cp /home/ubuntu/jktech/gunicorn/gunicorn.service /etc/systemd/system/gunicorn.service
 
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn.socket
+sudo systemctl enable gunicorn.socket
 sudo systemctl start gunicorn.service
 sudo systemctl enable gunicorn.service
--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/jktech/jktech/settings.py
+sed -i 's/\[]/\["3.87.108.126"]/' /home/ubuntu/jktech/jktech/settings.py
 
 python manage.py migrate
 python manage.py makemigrations
