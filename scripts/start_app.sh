afcced5df48516b6d24b0f9150e925e4728f2950
rrsingh <rsingh@infoane.com>
2024-08-08 18:58:10 +0530

    Updated files

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-sed -i 's/\[]/\["54.173.177.44"]/' /home/ubuntu/books_reviews/jktech/settings.py
+sed -i 's/\[]/\["52.23.220.101"]/' /home/ubuntu/books_reviews/jktech/settings.py
 
 python manage.py migrate
 python manage.py makemigrations
