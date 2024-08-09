263595ba472bd3bd61ea9dc3b07a56dcea6b711c
rrsingh <rsingh@infoane.com>
2024-08-09 19:12:14 +0530

    Updated

--- jktech/urls.py
+++ jktech/urls.py
@@ -5,6 +5,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 schema_view = get_schema_view(
@@ -27,4 +29,4 @@ urlpatterns = [
     path('books_review/', include('jktech_app.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
