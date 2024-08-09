e76910011f4f29dd0318cab51e6e410e8fac2279
rrsingh <rsingh@infoane.com>
2024-08-09 18:59:32 +0530

    Updated

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -4,7 +4,7 @@ sed -i 's/\[]/\["52.23.220.101"]/' /home/ubuntu/books_reviews/jktech/settings.py
 
 python manage.py migrate
 python manage.py makemigrations
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 sudo service gunicorn restart
 sudo service nginx restart
 
